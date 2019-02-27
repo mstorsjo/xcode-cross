e6d752873c860c9b9fcfab4bd9becb2c04f6ca64
Martin Storsjö <martin@martin.st>
2019-02-27 14:05:27 +0200

    Keep the tvOS and watchOS SDKs

--- strip-xcode.sh
+++ strip-xcode.sh
@@ -30,34 +30,39 @@ rm -rf Toolchains
 mv Toolchains-new Toolchains
 
 cd Platforms
-rm -rf AppleTV* Watch*
 cd MacOSX.platform
 rm -rf usr _CodeSignature
 cd Developer/SDKs/MacOSX10.*sdk
 rm -rf usr/share
 cd ../../../..
 
-cd iPhoneOS.platform
-rm -rf DeviceSupport usr _CodeSignature Developer/Library/CoreSimulator
-cd ..
+for sdk in iPhone Watch AppleTV; do
+	OS=${sdk}OS
+	SIM=${sdk}Simulator
+	cd $OS.platform
+	rm -rf DeviceSupport usr _CodeSignature Developer/Library/CoreSimulator
+	cd ..
 
-cd iPhoneSimulator.platform
-rm -rf Developer/Library/CoreSimulator Developer/Library/Frameworks Developer/Library/PrivateFrameworks _CodeSignature
-cd Developer/SDKs/iPhoneSimulator.sdk
-rm -rf usr/share usr/libexec Library Developer Applications
-# On Xcode 9.x and newer, usr/lib contains tbd files for the libraries, and
-# linking to them for simulator builds succeeds. On Xcode 8.x and older,
-# the usr/lib dir contains large dylib files, and linking against them doesn't
-# succeed anyway (it's missing /usr/lib/system/libsystem_kernel.dylib).
-# Thus remove the large files from older SDKs, while keeping enough for
-# linking to succeed on newer SDks.
-rm -rf usr/lib/system usr/lib/*.dylib
-if [ ! -e System/Library/Frameworks/Foundation.framework/Foundation.tbd ]; then
-	# Xcode 7.x and 8.x has got full frameworks for the simulator here,
-	# replace them with thin frameworks with TBD files for the target.
-	# Xcode 9 has got TBD files for the simulator as well.
-	# On Xcode 9/iOS 11 SDK, building for the simulatorh with header
-	# files taken from the target breaks.
-	rm -rf System
-	cp -a ../../../../iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System .
-fi
+	cd $SIM.platform
+	rm -rf Developer/Library/CoreSimulator Developer/Library/Frameworks Developer/Library/PrivateFrameworks _CodeSignature
+	cd Developer/SDKs/$SIM.sdk
+	rm -rf usr/share usr/libexec Library Developer Applications
+	# On Xcode 9.x and newer, usr/lib contains tbd files for the libraries,
+	# and linking to them for simulator builds succeeds. On Xcode 8.x and
+	# older, the usr/lib dir contains large dylib files, and linking
+	# against them doesn't succeed anyway (it's missing
+	# /usr/lib/system/libsystem_kernel.dylib). Thus remove the large files
+	# from older SDKs, while keeping enough for linking to succeed on
+	# newer SDks.
+	rm -rf usr/lib/system usr/lib/*.dylib
+	if [ ! -e System/Library/Frameworks/Foundation.framework/Foundation.tbd ]; then
+		# Xcode 7.x and 8.x has got full frameworks for the simulator here,
+		# replace them with thin frameworks with TBD files for the target.
+		# Xcode 9 has got TBD files for the simulator as well.
+		# On Xcode 9/iOS 11 SDK, building for the simulatorh with header
+		# files taken from the target breaks.
+		rm -rf System
+		cp -a ../../../../$OS.platform/Developer/SDKs/$OS.sdk/System .
+	fi
+	cd ../../../..
+done
